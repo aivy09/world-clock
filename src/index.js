728b4cc4f5143041a9b8a3ecc56af13db1c5c466
aivy09 <147442706+aivy09@users.noreply.github.com>
2024-02-21 13:38:59 -0800

    Added home link

--- src/index.js
+++ src/index.js
@@ -51,9 +51,8 @@ function updateCity(event){
 						<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
 					</div>
 					<div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-				</div>`;
-
-    <a href="/">All Cities</a>;
+                    </div>
+                    <a href="/" class="home">All Cities</a>`
 }
 
 
